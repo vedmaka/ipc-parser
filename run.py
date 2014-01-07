6d6123ea9864ce4b243d0129ad5f10eb8eeec455
Yury Katkov <katkov.juriy@gmail.com>
2014-01-07 05:51:27 +0400

    Merge branch 'master' of https://github.com/vedmaka/ipc-parser categories are not being created
    Conflicts:
    	run.py

--- run.py
+++ run.py
@@ -13,62 +13,66 @@ from ipc import IpcParser
 from collections import Counter
 import re
 
-ipc = IpcParser()
-entries = ipc.parse('ipcr.xml')
-ipc.build_indexes('localhost', 'ipc', 'mysql', 'mysql')
-
-wf = mwclient.Site('en.wikioffuture.org')
-page = wf.Pages['Agriculture and Empire in the High-Altitude Atacama Desert']
-
-pageText = page.edit()
-parsed = mwp.parse(pageText)
-template = parsed.filter_templates()[0]
-
-
-print "Done!"
-abstract = template.get('Abstract').value.lower()
-
-print [x.title for x in entries]
-print [x.title for x in entries[0].children]
-print [x.title for x in entries[0].children[0].children]
-print [x.title for x in entries[0].children[0].children[0].children]
-print [x.title for x in entries[0].children[0].children[0].children[0].children]
-
 
 def createCategory(name, parent):
     category = pywikibot.Page(pywikibot.getSite(), name)
     if not parent:
         category.put("[[Category:IPCT topics]]")
     else:
-        category.put("[[Category:"+parent+"]]")
+        category.put("[[Category:" + parent + "]]")
 
 def recursiveCreateCategories(category, parentName):
-    if not category.childen:
-        createCategory(category.title, parentName)
+    """
+
+    :type category: IpcEntry
+    """
+    if category.kind=="i" or \
+            not hasattr(category, 'symbol') or \
+            not hasattr(category, 'title'):
+        return
+
+    if not category.children:
+        createCategory("Category:"+category.symbol + ". " + category.title, parentName)
     else:
-        createCategory(category.title, parentName)
+        createCategory("Category:"+category.symbol + ". " + category.title, parentName)
         for i in category.children:
             recursiveCreateCategories(i, category.title)
 
+#PHASE 1 - BUILDING CATEGORY PAGES
+ipc = IpcParser()
+entries = ipc.parse('ipcr.xml')
 root = pywikibot.Page(pywikibot.getSite(), "Category:IPCT topics")
 root.put("This is core category for all topics in IPCT")
-
 [recursiveCreateCategories(x, "Category:IPCT topics") for x in entries]
 
-indexes = ipc.get_indexes('localhost', 'ipc', 'mysql', 'mysql')
-found = []
-
-for index in indexes:
-    #print 'Trying '+index.word
-    word = index.word.lower()
-    count = abstract.count(word)
-    if not len(word):
-        print "0-WORD!"
-        continue
-    if count:
-        print 'Found index id: '+str(index.id)+' '+str(count)+' times.'
-        found.append([index, count])
-
-print 'Page will be set with categories:'
-for f in found:
-    print str(f[1])+' / '+f[0].code + ' ' + f[0].category
+#
+# #PHASE 2 BUILDING INDEXES and
+# ipc.build_indexes('localhost', 'ipc', 'mysql', 'mysql')
+# wf = mwclient.Site('en.wikioffuture.org')
+#
+#
+# # PHASE 3 ADDING CATEGORIES TO WIKIPAGE
+# page = wf.Pages['Agriculture and Empire in the High-Altitude Atacama Desert']
+# pageText = page.edit()
+# parsed = mwp.parse(pageText)
+# template = parsed.filter_templates()[0]
+#
+# abstract = template.get('Abstract').value.lower()
+#
+# indexes = ipc.get_indexes('localhost', 'ipc', 'mysql', 'mysql')
+# found = []
+#
+# for index in indexes:
+#     #print 'Trying '+index.word
+#     word = index.word.lower()
+#     count = abstract.count(word)
+#     if not len(word):
+#         print "0-WORD!"
+#         continue
+#     if count:
+#         print 'Found index id: ' + str(index.id) + ' ' + str(count) + ' times.'
+#         found.append([index, count])
+#
+# print 'Page will be set with categories:'
+# for f in found:
+#     print str(f[1]) + ' / ' + f[0].code + ' ' + f[0].category
